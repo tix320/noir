8d1c873ebe1432e507ecad6045b92f89649201be
Tigran Sargsyan <tixtix320@gmail.com>
2022-08-18 11:28:12 +0300

    Delete prod-start params.

--- scripts/prod-start.ps1
+++ scripts/prod-start.ps1
@@ -1,15 +1,3 @@
-Param(
-    [int]
-    [Parameter(Mandatory = $true)]
-    $ServerPort,
-
-    [String]
-    [Parameter(Mandatory = $true)]
-    $DBAddress
-)
-
-$env:SERVER_PORT=$ServerPort
-$env:DB_HOST=$DBAddress
 $env:WEB_CLIENT_BUNDLE="../react-client/build"
 
 $env:NODE_ENV="production"
