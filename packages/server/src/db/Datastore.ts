76491330511b56d3fe57cb734627a80c3c64f37a
Tigran Sargsyan <tixtix320@gmail.com>
2022-08-23 12:08:41 +0400

    Imrpove DB connection mechanism.

--- packages/server/src/db/Datastore.ts
+++ packages/server/src/db/Datastore.ts
@@ -1,13 +1,26 @@
-import { connect } from 'mongoose';
+import mongoose from 'mongoose';
 
-const DB_HOST = process.env.DB_HOST
-console.info(`DB_HOST=${DB_HOST}`)
+const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+console.info(`DB_CONNECTION_STRING=${DB_CONNECTION_STRING}`)
 
+const connectionString = DB_CONNECTION_STRING ?? 'mongodb://localhost:27017/noir'
 
-async function run() {
-    const host = DB_HOST ?? 'localhost:27017'
-    await connect(`mongodb://${host}/noir`);
-
+async function connect() {
+    return await mongoose.connect(connectionString, {
+        connectTimeoutMS: 5000
+    });
 }
 
-run().then(() => console.info('DB connection established.')).catch(err => console.error(err));
+async function openInitialConnection() {
+    connect().catch(err => {
+        console.error(err);
+        openInitialConnection();
+    })
+}
+
+mongoose.connection.on('connecting', () => console.info("DB: Opening initial connection..."));
+mongoose.connection.once('connected', () => console.info("DB: Connected."));
+mongoose.connection.on('reconnected', () => console.info("DB: Reconnected."));
+mongoose.connection.on('disconnected', () => console.info("DB: Disconnected."));
+
+openInitialConnection();
